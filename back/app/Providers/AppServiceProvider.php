1f01767c1061dde58611824f8962be8de2c355db
Gediminas <neniusgediminas@gmail.com>
2023-03-04 19:28:47 +0200

    Injected CryptoCurrencyRepository into container

--- back/app/Providers/AppServiceProvider.php
+++ back/app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Interfaces\ICryptoCurrencyRepository;
+use App\Repositories\CryptoCurrencyRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(ICryptoCurrencyRepository::class, CryptoCurrencyRepository::class);
     }
 
     /**
