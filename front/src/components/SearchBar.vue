7828c9eacd7e4c16d91790f631a912e37ef5f7d7
Gediminas <neniusgediminas@gmail.com>
2023-03-20 17:49:58 +0200

    Created SearchBar.vue

---
+++ front/src/components/SearchBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="row">
+    <div class="col-md-6">
+      <div class="form-group">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Search"
+          v-model="searchQuery"
+          @keyup="debouncedSearchCryptoCurrencies()"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import debounce from 'lodash/debounce'
+
+export default {
+  data() {
+    return {
+      searchQuery: ''
+    }
+  },
+  methods: {
+    debouncedSearchCryptoCurrencies: debounce(function (this: any) {
+      this.$emit('search', this.searchQuery)
+    }, 500)
+  },
+  emits: ['search']
+}
+</script>
